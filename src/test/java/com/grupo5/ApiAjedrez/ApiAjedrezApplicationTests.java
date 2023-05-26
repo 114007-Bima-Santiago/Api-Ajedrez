8b711d5cd001233e98e0a65a267df5c41196786d
114007-Bima-Santiago114007@tecnicatura.frc.utn.edu.ar <you@example.com>
2023-05-26 11:51:51 -0300

    inicializacion del proyecto

---
+++ src/test/java/com/grupo5/ApiAjedrez/ApiAjedrezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo5.ApiAjedrez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAjedrezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
