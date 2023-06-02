fb1abd9b791890152aacb19101b7ea77c45ef386
114007-Bima-Santiago114007@tecnicatura.frc.utn.edu.ar <you@example.com>
2023-06-02 11:39:19 -0300

    creación de coordenadas y uso en el controller de juego

--- src/main/java/com/grupo5/ApiAjedrez/Controllers/JuegoController.java
+++ src/main/java/com/grupo5/ApiAjedrez/Controllers/JuegoController.java
@@ -1,4 +1,5 @@
 package com.grupo5.ApiAjedrez.Controllers;
+import com.grupo5.ApiAjedrez.Dominio.Coordenada;
 import com.grupo5.ApiAjedrez.Dominio.Partida;
 import com.grupo5.ApiAjedrez.Dto.Touch;
 import com.grupo5.ApiAjedrez.Juego;
@@ -12,10 +13,12 @@ public class JuegoController {
     @Autowired
     public JuegoController(){  }
 
-    @GetMapping("obtener/touch")
-    public Touch getTouch(){
+    @GetMapping("obtener/touch/{fA}{cA}-{fN}{cN}")
+    public Touch getTouch(@PathVariable int fA, @PathVariable int cA, @PathVariable int fN, @PathVariable int cN){
         juego = Juego.obtenerInstancia();
-        return juego.mov();
+        Coordenada casillaAntigua = new Coordenada(fA, cA);
+        Coordenada casillaNueva = new Coordenada(fN, cN);
+        return juego.mov(casillaAntigua, casillaNueva);
     }
 
     @GetMapping("nueva-partida/{idJB}/{idJN}")
---
+++ src/main/java/com/grupo5/ApiAjedrez/Dominio/Coordenada.java
@@ -0,0 +1,13 @@
+package com.grupo5.ApiAjedrez.Dominio;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coordenada {
+    private int fila;
+    private int columna;
+}
