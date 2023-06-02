852dbec49edb61991fad7aec56c7648c9d09de46
114007-Bima-Santiago114007@tecnicatura.frc.utn.edu.ar <you@example.com>
2023-06-02 11:30:43 -0300

    metodos para calcular los movimientos de las piezas

--- src/main/java/com/grupo5/ApiAjedrez/Dominio/Pieza.java
+++ src/main/java/com/grupo5/ApiAjedrez/Dominio/Pieza.java
@@ -3,6 +3,8 @@ package com.grupo5.ApiAjedrez.Dominio;
 
 import lombok.*;
 
+import java.util.List;
+
 @Getter
 @Setter
 @ToString
@@ -10,4 +12,14 @@ import lombok.*;
 @AllArgsConstructor
 public abstract class Pieza {
     private int id;
+    public List<int[]> movPosibles(int[][] tablero, int[] posicion) {
+        return null;
+    }
+
+    public void borrarPosibles(int[][] tablero, List<int[]> movimientosPosibles){
+        for(int[] movimientoPosible : movimientosPosibles){
+            tablero[movimientoPosible[0]][movimientoPosible[1]] -= 100;
+        }
+
+    }
 }
