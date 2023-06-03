e0808a61bd59151c5da1e9209f4799eddd615fee
114007-Bima-Santiago114007@tecnicatura.frc.utn.edu.ar <you@example.com>
2023-06-03 14:20:22 -0300

    corrección de errores

--- src/main/java/com/grupo5/ApiAjedrez/Dominio/Pieza.java
+++ src/main/java/com/grupo5/ApiAjedrez/Dominio/Pieza.java
@@ -11,14 +11,13 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public abstract class Pieza {
-    private int id;
     public List<Coordenada> movPosibles(int[][] tablero, Coordenada posicion) {
         return null;
     }
 
     public void borrarPosibles(int[][] tablero, List<Coordenada> movimientosPosibles){
         for(Coordenada movimientoPosible : movimientosPosibles){
-            tablero[movimientoPosible.getFila()][movimientoPosible.getColumna()] -= 100;
+            tablero[movimientoPosible.getColumna()][movimientoPosible.getFila()] -= 100;
         }
 
     }
