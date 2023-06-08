4eb09bc80e5834f6352f15a1577db10a12293981
114007-Bima-Santiago114007@tecnicatura.frc.utn.edu.ar <you@example.com>
2023-06-07 23:27:39 -0300

    creacion de metodos necesarios para el calculo del codigo del touch (solo para peon)

--- src/main/java/com/grupo5/ApiAjedrez/Dominio/Partida.java
+++ src/main/java/com/grupo5/ApiAjedrez/Dominio/Partida.java
@@ -1,6 +1,8 @@
 package com.grupo5.ApiAjedrez.Dominio;
 
 
+import com.grupo5.ApiAjedrez.Dominio.Piezas.*;
+import com.grupo5.ApiAjedrez.Dto.Touch;
 import lombok.*;
 
 import javax.persistence.*;
@@ -31,15 +33,90 @@ public class Partida implements Serializable {
     @Column
     private int turnoOponente;
     @Column
-    private int contenidoPosVieja;
-    @Column
-    private int contenidoPosActual;
-    @Column
     private List<String> listaMovimientos;
+    @Column
+    private boolean segundoTouch;
 
     //TODO: ver si hay que incluir foreign keys
     @Column
     private Jugador jugadorBlancas;
     @Column
     private Jugador jugadorNegras;
+
+
+    private static List<String> movimientosPosibles;
+    private static Coordenada posVieja;
+
+    public Touch getTouch(Coordenada coord){
+        int contenidoEscaque = getContenidoEscaque(coord);
+
+        if (!segundoTouch && contenidoEscaque / 10 == turno) {
+            Pieza pieza;
+            switch (contenidoEscaque % 10){
+                case 4:
+                    pieza = new Peon();
+                    break;
+                case 5:
+                    pieza = new Torre();
+                    break;
+                case 6:
+                    pieza = new Caballo();
+                    break;
+                case 7:
+                    pieza = new Alfil();
+                    break;
+                case 8:
+                    pieza = new Dama();
+                    break;
+                case 9:
+                    pieza = new Rey();
+                    break;
+                default:
+                    pieza = null;
+                    break;
+            }
+
+            movimientosPosibles = pieza.movPosibles(tablero.getTablero(), coord);
+            posVieja = coord;
+            segundoTouch = true;
+            return new Touch(1);
+        } else if (segundoTouch && contenidoEscaque >= 100) {
+            borrarPosibles(movimientosPosibles);
+            movimiento(posVieja, coord);
+            segundoTouch = false;
+            cambiarTurno();
+            return new Touch(2);
+        } else {
+            return new Touch(3);
+        }
+    }
+
+    private void cambiarTurno() {
+        if (turno == 2) {
+            turno = 3;
+        } else {
+            turno = 2;
+        }
+    }
+
+    public int getContenidoEscaque(Coordenada coord) { return tablero.getPosicion(coord.getColumna(), coord.getFila()); }
+
+    public void borrarPosibles(List<String> movimientosPosibles) {
+        for (String movimientoPosible : movimientosPosibles) {
+            Coordenada coord = new Coordenada();
+            coord.transformarACoordenada(movimientoPosible);
+
+            tablero.getTablero()[coord.getColumna()][coord.getFila()] -= 100;
+        }
+    }
+
+    public void movimiento(Coordenada posicion1, Coordenada posicion2) {
+        int contenido = getContenidoEscaque(posicion1);
+        setContenidoEscaque(posicion2, contenido);
+        setContenidoEscaque(posicion1, 0);
+    }
+
+    public void setContenidoEscaque(Coordenada posicion, int contenido) {
+        tablero.getTablero()[posicion.getColumna()][posicion.getFila()] = contenido;
+    }
 }
